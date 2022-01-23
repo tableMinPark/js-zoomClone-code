f65dc82d961d27428e99610b14945f18cb30c85f
ParkSangmin <tablemin_park@daum.net>
2022-01-24 00:31:33 +0900

    1.0 Introduction ~ 1.5 Recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,22 @@
-alert("test");
+// socket : 연결을 위한 서버의 소켓
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 서버로의 연결이 열렸을 때 이벤트 
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+// 서버로부터의 메시지가 수신되었을 때 이벤트
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data, " from the server!");
+});
+
+// 서버로의 연결이 닫혔을 때 이벤트
+socket.addEventListener("close", () => {    
+    console.log("Disconnected to Server ❌");
+});
+
+// 서버로의 메시지 전송 (연결 후 5초후)
+setTimeout(() => {
+    socket.send("hello!");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,5 +10,31 @@ app.use("/public", express.static(__dirname + "/public"));  // 유저가 /public
 
 app.get("/", (req, res) => res.render("home"));             // '/' 로 들어오는 get 요청을 home.pug 로 연결
 app.get("/*", (req, res) => res.redirect("home"));          // '/' 로 시작하는 모든 요청들을 home.pug 로 리다이렉트
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);     // 3000 번 port 포트 오픈
+
+const server = http.createServer(app);      // http server 생성
+const wss = new WebSocket.Server({ server });         // web socket server 생성 후 http server 병합 (3000 port 로 http 와 web socket 을 둘다 구동할 때 필요)
+
+// 서버와 연결된 각 브라우저들을 위한 코드
+wss.on("connection", (socket) => {
+    // socket : 서버에 연결된 브라우저의 소켓
+
+    // 브라우저와의 연결이 되었을 때 출력
+    console.log("Connected to Browser ✅")
+
+    // 브라우저와의 연결이 닫혔을 때 이벤트
+    socket.on("close", () => {
+        console.log("Disconnected to Browser ❌");
+    });
+
+    // 브라우저부터의 메시지가 수신되었을 때 이벤트
+    socket.on("message", (message) =>{
+        console.log("New message: ", message.toString('utf8'), " from the browser!");
+    });
+
+    // 브라우저로의 메시지 전송 (연결 직후)
+    socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
